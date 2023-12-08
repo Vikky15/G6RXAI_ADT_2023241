a4d1676b28e68f3baeabc3cc3b1b55842eefccb8
Vikky15 <obasivictoria@stud.uni-obuda.hu>
2023-12-08 01:13:17 +0100

    Controller

--- G6RXAI_ADT_2023241.Endpoint/Program.cs
+++ G6RXAI_ADT_2023241.Endpoint/Program.cs
@@ -1,15 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 namespace G6RXAI_ADT_2023241.Endpoint
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<StartupBase>();
+                });
     }
 }
