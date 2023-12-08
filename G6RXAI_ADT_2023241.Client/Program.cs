657e3f3fc45480bd70e07c2417b4d8c3de0d68b3
Vikky15 <obasivictoria@stud.uni-obuda.hu>
2023-12-08 01:32:31 +0100

    Client

--- G6RXAI_ADT_2023241.Client/Program.cs
+++ G6RXAI_ADT_2023241.Client/Program.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 
-namespace G6RXAI_ADT_2023241.Client 
+namespace G6RXAI_ADT_2023241.Client
+{     static class Extension
 {
+    public static void ToProcess<T>(this IEnumerable<T> query, string name)
+    {
+        Console.WriteLine($"\n:: {name} ::\n");
+
+        foreach (var item in query)
+            Console.WriteLine("\t" + item);
+
+        Console.WriteLine("\n\n");
+    }
+}
     internal class Program
     {
         static void Main(string[] args)
